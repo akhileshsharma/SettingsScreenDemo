d8b2e2aaadd651967375b5b631bb0879733b7315
Mirko Cukich <mirkocukich@gmail.com>
2018-01-25 17:33:29 -0600

    activity_main.xml & MainActivity.java complete

--- app/src/main/java/com/digitalmirko/settingsscreendemo/MainActivity.java
+++ app/src/main/java/com/digitalmirko/settingsscreendemo/MainActivity.java
@@ -1,15 +1,29 @@
 package com.digitalmirko.settingsscreendemo;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btn_settings;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn_settings = (Button) findViewById(R.id.btn_settings);
+
+        btn_settings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), SettingsActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
